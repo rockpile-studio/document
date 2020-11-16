3fabdb9f655cf5f09c5138368b8ffd4caeeba0fc
linlei <llei1@linewell.com>
2020-11-16 23:27:00 +0800

    提交更新

--- learning-vue/案例/ShoppingCart/main.js
+++ learning-vue/案例/ShoppingCart/main.js
@@ -5,26 +5,53 @@ const app = new Vue({
       {
         id: 50301,
         name: 'unix编程指南',
-        price: 100, count: 0,
+        price: 100.50,
+        count: 1,
         publishDate: '2020-01-01'
       },
       {
         id: 50302,
         name: 'java编程思想',
-        price: 200,
-        count: 0,
+        price: 200.80,
+        count: 1,
         publishDate: '2020-01-01'
       },
       {
         id: 50303,
         name: '小学语文一年级',
-        price: 300,
-        count: 0,
+        price: 300.00,
+        count: 1,
         publishDate: '2020-01-01'
       },
-    ],
-    totalPrice: 0,
+    ]
   },
   methods: {
-  }
+    increment(index) {
+      this.books[index].count++;
+    },
+    decrement(index) {
+      this.books[index].count--;
+    },
+    remove(index) {
+      this.books.splice(index, 1);
+    }
+  },
+  computed: {
+    totalPrice() {
+      let totalPrice = 0.0;
+      // for( let i=0; i<this.books.length; i++ ){
+      //   totalPrice = totalPrice + this.books[i].price * this.books[i].count;
+      // }
+      // 通过JS高阶函数的方式简化代码
+      totalPrice = this.books.reduce((total,n) => (total += n.price * n.count), 0);
+      return totalPrice;
+    },
+  },
+  // 使用过滤器的方式对价格进行格式化等操作
+  filters: {
+    showPrice(price) {
+      // .toFixed(2)保留两位小数
+      return '￥' + price.toFixed(2);
+    },
+  },
 });
