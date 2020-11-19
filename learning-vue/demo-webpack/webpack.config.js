3e4a9567a96f2c0e618c83ecd055b673e4720079
linlei <llei1@linewell.com>
2020-11-19 18:17:57 +0800

    提交更新

--- learning-vue/demo-webpack/webpack.config.js
+++ learning-vue/demo-webpack/webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     // 动态获取文件的路径，通过nodejs的全局变量__dirname（对应当前文件所在的目录路径）
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
+    // 默认情况下，webpack会将生成的路径直接返回
+    // 但是整个程序是打包在dist目录下，所以publicPath添加"dist/"
+    publicPath: 'dist/',
   },
   module: {
     rules: [
@@ -14,6 +17,19 @@ module.exports = {
         // css-loader负责将css文件进行加载
         // style-loader负责将样式添加到dom中，style-loader必须放在css-loader左侧
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 当加载的图片小于limit(8192B)时，会将图片转换编译成base64字符串形式，
+              // 大于limit的使用file-loader加载
+              limit: 8192
+            }
+          }
+        ]
       }
     ]
   }
